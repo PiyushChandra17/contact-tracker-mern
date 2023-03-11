d9b540c591d47d6076046895b822731f40c11006
PiyushChandra17 <chandra.piyush17@gmail.com>
2023-03-11 21:54:46 +0530

    updated package.json

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -29,7 +29,7 @@ const ContactState = props => {
     const getContacts = async () => {
 
         try {
-            const res = await axios.get('http://localhost:5000/api/contacts')
+            const res = await axios.get('/api/contacts')
 
             dispatch({ type: GET_CONTACTS, payload: res.data })
 
@@ -49,7 +49,7 @@ const ContactState = props => {
         }
 
         try {
-            const res = await axios.post('http://localhost:5000/api/contacts', contact, config)
+            const res = await axios.post('/api/contacts', contact, config)
 
             dispatch({ type: ADD_CONTACT, payload: res.data })
 
@@ -62,7 +62,7 @@ const ContactState = props => {
     // Delete Contact
     const deleteContact = async (id) => {
         try {
-            await axios.delete(`http://localhost:5000/api/contacts/${id}`)
+            await axios.delete(`/api/contacts/${id}`)
 
             dispatch({ type: DELETE_CONTACT, payload: id })
 
@@ -82,7 +82,7 @@ const ContactState = props => {
         }
 
         try {
-            const res = await axios.put(`http://localhost:5000/api/contacts/${contact._id}`, contact, config)
+            const res = await axios.put(`/api/contacts/${contact._id}`, contact, config)
 
             dispatch({ type: UPDATE_CONTACT, payload: res.data })
 
